f200ae5f08c2881d9d231f7d469d8dd0acfd61d9
THONG NGUYEN <thongnguyentam2k4@gmail.com>
2021-08-29 17:52:35 +0700

    lol

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Products from './Components/Products';
+import Banner from './Components/Banner';
+import Nav_bar from './Components/Nav_bar';
+import Profile from './Components/Profile';
+import Login from './Components/Login';
+import Upload from "./Components/Upload";
+import Items from "./Components/Items";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+function App() {
+    return (
+      <Router>
+        <Nav_bar />
+        <Banner />
+        <Switch>
+          <Route path="/home" exact component={Products} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/account" exact component={Profile} />
+          <Route path="/" exact component={Login}/>
+          <Route path="/sell" exact component={Upload}/>
+        </Switch>
+        <Items />
+      </Router>
+    );
+}
+
+export default App;
